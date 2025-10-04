1735e494a493ce95f4190a82135eba86d2a62f89
mahdirnma <rahnamamahdi78@gmail.com>
2025-10-04 19:36:12 +0330

    Initial commit

---
+++ app/Http/Controllers/CollegianController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Collegian;
+use App\Http\Requests\StoreCollegianRequest;
+use App\Http\Requests\UpdateCollegianRequest;
+
+class CollegianController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCollegianRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Collegian $collegian)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Collegian $collegian)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCollegianRequest $request, Collegian $collegian)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Collegian $collegian)
+    {
+        //
+    }
+}
